4f08e8cda082c5a79bea455b7c8047cdf780fded
Tyler Richey <iam@tylerrichey.com>
2020-06-21 12:00:17 -0700

    fix js

--- src/js/matomo-tracking.js
+++ src/js/matomo-tracking.js
@@ -7,11 +7,11 @@ const matomoSiteId = document
 
 var _paq = _paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(['disableCookies']);
+//_paq.push(['disableCookies']);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  _paq.push(['setTrackerUrl', `${matomoUrl}/piwik.php`]);
+  _paq.push(['setTrackerUrl', `${matomoUrl}/matomo.php`]);
   _paq.push(['setSiteId', matomoSiteId]);
 
   const d = document;
@@ -20,6 +20,6 @@ _paq.push(['enableLinkTracking']);
 
   g.type = 'text/javascript';
   g.defer = true;
-  g.src = `${matomoUrl}/piwik.js`;
+  g.src = `${matomoUrl}/matomo.js`;
   s.parentNode.insertBefore(g, s);
 })();
